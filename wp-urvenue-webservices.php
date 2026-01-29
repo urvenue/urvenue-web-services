002f7f0898df4766fbe198a3c3660b31232a4d35
Nicolás Zamora <nz@urvenue.com>
2026-01-28 18:07:44 -0600

    Update slug and version

--- wp-urvenue-webservices.php
+++ wp-urvenue-webservices.php
@@ -3,11 +3,21 @@
 /**
  * @wordpress-plugin
  * Plugin Name: UrVenue Web Services
+<<<<<<< Updated upstream
  * Version:     1.0.52
  * Plugin URI:  https://wordpress.org/plugins/urvenue-web-services/
  * Description: UrVenue Integrations: Events, Inventory.
  * Author:      UrVenue/uws
  * Author URI:  https://www.urvenue.com/
+=======
+ * Plugin URI:  https://wordpress.org/plugins/wp-urvenue-webservices/
+ * Description: UrVenue Integrations: Events, Inventory.
+ * Version:     1.2.1
+ * Author:      UrVenue / UWS
+ * Author URI:  https://www.urvenue.com/
+ * License:     GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+>>>>>>> Stashed changes
  * Text Domain: wp-urvenue-webservices
  */
 
