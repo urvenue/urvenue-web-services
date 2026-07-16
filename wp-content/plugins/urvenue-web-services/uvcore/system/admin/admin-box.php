37b0d398d94b126fb7c7eae5636ce7636a07a876
Axl Eduardo Guillen Luna <axl.guillen@urvenue.com>
2026-07-16 15:48:23 -0600

    Remove task migration comments from admin.php, admin-box, admin-pages and proxy

--- wp-content/plugins/urvenue-web-services/uvcore/system/admin/admin-box.php
+++ wp-content/plugins/urvenue-web-services/uvcore/system/admin/admin-box.php
@@ -2,28 +2,18 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 global $urvenue_ws_adm_adminbox_tabs;
 
-// $urvenue_ws_admin_optstabs_state = uvs_get_boxtabs_state($urvenue_ws_initialtab);
-// $urvenue_ws_admin_optstabs_state = urvenue_ws_adm_get_boxtabs_state($urvenue_ws_initialtab); // Axl UWS-7416
-$urvenue_ws_admin_optstabs_state = urvenue_ws_adm_get_boxtabs_state($urvenue_ws_initialtab); // Axl UWS-7634
-// $urvenue_ws_boxpanelclass = (is_array($urvenue_ws_core_lib) and !$urvenue_ws_core_lib["system"]["apikey"]) ? "uvapiconfig" : "";
-$urvenue_ws_boxpanelclass = (is_array($urvenue_ws_core_lib) and !$urvenue_ws_core_lib["system"]["apikey"]) ? "uvapiconfig" : ""; // Axl UWS-7634
+$urvenue_ws_admin_optstabs_state = urvenue_ws_adm_get_boxtabs_state($urvenue_ws_initialtab);
+$urvenue_ws_boxpanelclass = (is_array($urvenue_ws_core_lib) and !$urvenue_ws_core_lib["system"]["apikey"]) ? "uvapiconfig" : "";
 
 ?>
 
-<?php // @Axl ?>
-<?php /* old: <div class="uvs-boxpanel uvs-boxpanel-admin [echo $urvenue_ws_boxpanelclass]"> */ ?>
-<?php /* old: <form id="uvs-uvcoreadmin-form" action="[echo $urvenue_ws_adm_admin_lib[loads][adminsave]]"> */ ?>
-<?php /* old: <div class="uvs-adminbox-corever">[echo $urvenue_ws_adm_core_version]</div> */ ?>
 <div class="uvs-boxpanel uvs-boxpanel-admin <?php echo esc_attr( $urvenue_ws_boxpanelclass ); ?>">
 	<form id="uvs-uvcoreadmin-form" action="<?php echo esc_url( $urvenue_ws_adm_admin_lib["loads"]["adminsave"] ); ?>">
 		<div class="uvs-adminbox-head uvs-clearfix">
 			<div class="uvs-adminbox-head-title">UrVenue Web Services</div>
 			<div class="uvs-adminbox-corever"><?php echo esc_html( $urvenue_ws_adm_core_version ); ?></div>
 		</div>
-<?php // @Axl End ?>
 
-		<?php // @egt [UWS-7297]
-		//wp_nonce_field('uvsp_adminsave_action', 'uvsp_adminsave_nonce'); ?>
 		<input class="uvsjson" type="hidden" name="uvsp_adminsave_nonce" value="<?php echo esc_attr( wp_create_nonce('uvsp_adminsave_action') ); ?>">
 		
 		<div class="uvs-adminbox-credentials">
@@ -32,46 +22,39 @@ $urvenue_ws_boxpanelclass = (is_array($urvenue_ws_core_lib) and !$urvenue_ws_cor
 
 		<div class="uvs-adminbox-admcols uvs-clearfix">
 			<div class="uvs-adminbox-mainmenu">
-				<?php // @Axl ?>
 			<ul>
-					<li class="uvs-menu-group"><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['dashboard']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['dashboard'] ); ?>" href="#dashboard"><span><i class="uwsicon-gauge"></i> Dashboard</span></a></li>
+					<li class="uvs-menu-group"><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['dashboard'] ); ?>" href="#dashboard"><span><i class="uwsicon-gauge"></i> Dashboard</span></a></li>
 
 					<li class="uvs-menu-group"><span><i class="uwsicon-calendar-1"></i> Events</span></li>
-					<li><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['events-global']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['events-global'] ); ?>" href="#events-global">Global</a></li>
-					<li class="uvissubitem"><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['events-calendar']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['events-calendar'] ); ?>" href="#events-calendar">Calendar</a></li>
-					<li class="uvissubitem"><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['events-agenda']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['events-agenda'] ); ?>" href="#events-agenda">Agenda</a></li>
+					<li><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['events-global'] ); ?>" href="#events-global">Global</a></li>
+					<li class="uvissubitem"><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['events-calendar'] ); ?>" href="#events-calendar">Calendar</a></li>
+					<li class="uvissubitem"><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['events-agenda'] ); ?>" href="#events-agenda">Agenda</a></li>
 					<?php /* old: <li class="uvissubitem"><a class="[echo $urvenue_ws_admin_optstabs_state[events-list]]" href="#events-list">List</a></li> */ ?>
-					<li class="uvissubitem"><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['events-slider']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['events-slider'] ); ?>" href="#events-slider">Slider</a></li>
-					<li><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['events-event']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['events-event'] ); ?>" href="#events-event">Event Page</a></li>
-					<li><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['flyers']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['flyers'] ); ?>" href="#flyers">Flyers</a></li>
+					<li class="uvissubitem"><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['events-slider'] ); ?>" href="#events-slider">Slider</a></li>
+					<li><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['events-event'] ); ?>" href="#events-event">Event Page</a></li>
+					<li><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['flyers'] ); ?>" href="#flyers">Flyers</a></li>
 					<?php /* old: <li class="uvs-menu-group"><span>Artists</span></li>
 					<li><a class="[echo $urvenue_ws_admin_optstabs_state[artists-artistpage]]" href="#artists-artistpage">Artist Page</a></li>
 					<li><a class="[echo $urvenue_ws_admin_optstabs_state[artists-list]]" href="#artists-list">Artists List</a></li> */ ?>
 
 					<li class="uvs-menu-group"><span><i class="uwsicon-cog"></i> Configuration</span></li>
-					<li><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['pages']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['pages'] ); ?>" href="#pages">Pages</a></li>
-					<li><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['venues']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['venues'] ); ?> uvs-menu-isvenues" href="#venues">Venues</a></li>
-					<li><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['inventory']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['inventory'] ); ?>" href="#inventory">Inventory</a></li>
-					<li><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['notifications']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['notifications'] ); ?>" href="#notifications">Notifications</a></li>
-					<li><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['map']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['map'] ); ?>" href="#map">Map</a></li>
-					<li><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state["ui-color-palette"]; */ echo esc_attr( $urvenue_ws_admin_optstabs_state["ui-color-palette"] ); ?> uvs-menu-isvenues" href="#ui-color-palette">UI Color Palette</a></li>
-					<li><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['seo']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['seo'] ); ?>" href="#seo">SEO</a></li>
-					<li><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['cache']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['cache'] ); ?>" href="#cache">Cache</a></li>
-					<li><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['api']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['api'] ); ?>" href="#api">API Info</a></li>
-					<li><a class="<?php /* Old: echo $urvenue_ws_admin_optstabs_state['status']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['status'] ); ?>" href="#status">System Status</a></li>
+					<li><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['pages'] ); ?>" href="#pages">Pages</a></li>
+					<li><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['venues'] ); ?> uvs-menu-isvenues" href="#venues">Venues</a></li>
+					<li><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['inventory'] ); ?>" href="#inventory">Inventory</a></li>
+					<li><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['notifications'] ); ?>" href="#notifications">Notifications</a></li>
+					<li><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['map'] ); ?>" href="#map">Map</a></li>
+					<li><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state["ui-color-palette"] ); ?> uvs-menu-isvenues" href="#ui-color-palette">UI Color Palette</a></li>
+					<li><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['seo'] ); ?>" href="#seo">SEO</a></li>
+					<li><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['cache'] ); ?>" href="#cache">Cache</a></li>
+					<li><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['api'] ); ?>" href="#api">API Info</a></li>
+					<li><a class="<?php echo esc_attr( $urvenue_ws_admin_optstabs_state['status'] ); ?>" href="#status">System Status</a></li>
 				</ul>
-			<?php // @Axl End ?>
 			</div>
 			<div class="uvs-adminbox-optionsarea">
 				<div class="uvs-adminbox-optionsarea-inner">
-					<?php // @Axl ?>
-					<?php /* old: <input ... value="[echo $urvenue_ws_core_lib[system][path]]"> */ ?>
-					<?php /* old: <input ... value="[echo $urvenue_ws_url]"> */ ?>
-					<?php /* old: <input ... value="[echo $urvenue_ws_core_lib[system][library]]"> */ ?>
 					<input class="uvsjson" type="hidden" name="system[path]" value="<?php echo esc_attr( $urvenue_ws_core_lib["system"]["path"] ); ?>">
 					<input class="uvsjson" type="hidden" name="system[url]" value="<?php echo esc_attr( $urvenue_ws_url ); ?>">
 					<input class="uvsjson" type="hidden" name="system[library]" value="<?php echo esc_attr( $urvenue_ws_core_lib["system"]["library"] ); ?>">
-					<?php // @Axl End ?>
 
 					<?php include_once($urvenue_ws_uvs_path . "/system/admin/admin-dashboard.php"); ?>
 					<?php include_once($urvenue_ws_uvs_path . "/system/admin/admin-events-global.php"); ?>
--- wp-content/plugins/urvenue-web-services/uvcore/system/admin/admin-pages.php
+++ wp-content/plugins/urvenue-web-services/uvcore/system/admin/admin-pages.php
@@ -1,104 +1,56 @@
 <?php
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-// $urvenue_ws_pageevents = uvs_get_adminfieldhtml("pages->events");
-// $urvenue_ws_pageevents = urvenue_ws_adm_get_adminfieldhtml("pages->events"); // Axl UWS-7416
-$urvenue_ws_pageevents = urvenue_ws_adm_get_adminfieldhtml("pages->events"); // Axl UWS-7634
-// $urvenue_ws_pagesingleevent = uvs_get_adminfieldhtml("pages->singleevent");
-// $urvenue_ws_pagesingleevent = urvenue_ws_adm_get_adminfieldhtml("pages->singleevent"); // Axl UWS-7416
-$urvenue_ws_pagesingleevent = urvenue_ws_adm_get_adminfieldhtml("pages->singleevent"); // Axl UWS-7634
-// $urvenue_ws_pagemap = uvs_get_adminfieldhtml("pages->map");
-// $urvenue_ws_pagemap = urvenue_ws_adm_get_adminfieldhtml("pages->map"); // Axl UWS-7416
-$urvenue_ws_pagemap = urvenue_ws_adm_get_adminfieldhtml("pages->map"); // Axl UWS-7634
-// $urvenue_ws_pageitem = uvs_get_adminfieldhtml("pages->itempage");
-// $urvenue_ws_pageitem = urvenue_ws_adm_get_adminfieldhtml("pages->itempage"); // Axl UWS-7416
-$urvenue_ws_pageitem = urvenue_ws_adm_get_adminfieldhtml("pages->itempage"); // Axl UWS-7634
-// $urvenue_ws_pagepriv = uvs_get_adminfieldhtml("pages->privacy");
-// $urvenue_ws_pagepriv = urvenue_ws_adm_get_adminfieldhtml("pages->privacy"); // Axl UWS-7416
-$urvenue_ws_pagepriv = urvenue_ws_adm_get_adminfieldhtml("pages->privacy"); // Axl UWS-7634
-// $urvenue_ws_pageterms = uvs_get_adminfieldhtml("pages->terms");
-// $urvenue_ws_pageterms = urvenue_ws_adm_get_adminfieldhtml("pages->terms"); // Axl UWS-7416
-$urvenue_ws_pageterms = urvenue_ws_adm_get_adminfieldhtml("pages->terms"); // Axl UWS-7634
+$urvenue_ws_pageevents = urvenue_ws_adm_get_adminfieldhtml("pages->events");
+$urvenue_ws_pagesingleevent = urvenue_ws_adm_get_adminfieldhtml("pages->singleevent");
+$urvenue_ws_pagemap = urvenue_ws_adm_get_adminfieldhtml("pages->map");
+$urvenue_ws_pageitem = urvenue_ws_adm_get_adminfieldhtml("pages->itempage");
+$urvenue_ws_pagepriv = urvenue_ws_adm_get_adminfieldhtml("pages->privacy");
+$urvenue_ws_pageterms = urvenue_ws_adm_get_adminfieldhtml("pages->terms");
 
-// $urvenue_ws_eventstext = (uvs_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_events]</strong></small>" : "";
-// $urvenue_ws_eventstext = (urvenue_ws_adm_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_events]</strong></small>" : ""; // Axl UWS-7416
-$urvenue_ws_eventstext = (urvenue_ws_adm_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_events]</strong></small>" : ""; // Axl UWS-7634
-// $urvenue_ws_eventtext = (uvs_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_event]</strong></small>" : "";
-// $urvenue_ws_eventtext = (urvenue_ws_adm_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_event]</strong></small>" : ""; // Axl UWS-7416
-$urvenue_ws_eventtext = (urvenue_ws_adm_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_event]</strong></small>" : ""; // Axl UWS-7634
-// $urvenue_ws_maptext = (uvs_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_map]</strong></small>" : "";
-// $urvenue_ws_maptext = (urvenue_ws_adm_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_map]</strong></small>" : ""; // Axl UWS-7416
-$urvenue_ws_maptext = (urvenue_ws_adm_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_map]</strong></small>" : ""; // Axl UWS-7634
-// $urvenue_ws_itempagetext = (uvs_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_inventory_item_page]</strong></small>" : "";
-// $urvenue_ws_itempagetext = (urvenue_ws_adm_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_inventory_item_page]</strong></small>" : ""; // Axl UWS-7416
-$urvenue_ws_itempagetext = (urvenue_ws_adm_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_inventory_item_page]</strong></small>" : ""; // Axl UWS-7634
+$urvenue_ws_eventstext = (urvenue_ws_adm_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_events]</strong></small>" : "";
+$urvenue_ws_eventtext = (urvenue_ws_adm_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_event]</strong></small>" : "";
+$urvenue_ws_maptext = (urvenue_ws_adm_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_map]</strong></small>" : "";
+$urvenue_ws_itempagetext = (urvenue_ws_adm_is_wordpress()) ? " <small>Shortcode: <strong>[urvenue_ws_inventory_item_page]</strong></small>" : "";
 ?>
-<div id="uvs-admin-pages" class="uvs-admin-opt-section <?php /* Old: echo $urvenue_ws_admin_optstabs_state['pages']; */ echo esc_attr( $urvenue_ws_admin_optstabs_state['pages'] ); ?>">
+<div id="uvs-admin-pages" class="uvs-admin-opt-section <?php echo esc_attr( $urvenue_ws_admin_optstabs_state['pages'] ); ?>">
     <div class="uvs-admin-opt-title">Integration Pages</div>
 	<div class="uvs-admin-opt-subtitle">Select the pages for your integrations, this will help for site internal links and redirections</div>
     <div class="uvs-admin-opt-space"></div>
 
 	<div class="uvs-infolist-item uvs-clearfix">
-		<?php /* Old: echo wp_kses( $urvenue_ws_eventstext, uvs_allowed_admin_html() ); */ ?>
-		<div class="uvsname">Events Page<?php /* Old: echo $urvenue_ws_eventstext; */ echo wp_kses( $urvenue_ws_eventstext, urvenue_ws_adm_allowed_admin_html() );  // Axl UWS-7416 ?></div>
+		<div class="uvsname">Events Page<?php echo wp_kses( $urvenue_ws_eventstext, urvenue_ws_adm_allowed_admin_html() ); ?></div>
 		<div class="uvsvalue">
-			<?php // @Axl ?>
-			<?php /* Old: echo $urvenue_ws_pageevents; */ ?>
-			<?php /* Old: echo wp_kses( $urvenue_ws_pageevents, uvs_allowed_admin_html() ); */ ?>
-			<?php echo wp_kses( $urvenue_ws_pageevents, urvenue_ws_adm_allowed_admin_html() );  // Axl UWS-7416 ?>
-			<?php // @Axl End ?>
+			<?php echo wp_kses( $urvenue_ws_pageevents, urvenue_ws_adm_allowed_admin_html() ); ?>
 		</div>
 	</div>
     <div class="uvs-infolist-item uvs-clearfix">
-		<?php /* Old: echo wp_kses( $urvenue_ws_eventtext, uvs_allowed_admin_html() ); */ ?>
-		<div class="uvsname">Event Page<?php /* Old: echo $urvenue_ws_eventtext; */ echo wp_kses( $urvenue_ws_eventtext, urvenue_ws_adm_allowed_admin_html() );  // Axl UWS-7416 ?></div>
+		<div class="uvsname">Event Page<?php echo wp_kses( $urvenue_ws_eventtext, urvenue_ws_adm_allowed_admin_html() ); ?></div>
 		<div class="uvsvalue">
-			<?php // @Axl ?>
-			<?php /* Old: echo $urvenue_ws_pagesingleevent; */ ?>
-			<?php /* Old: echo wp_kses( $urvenue_ws_pagesingleevent, uvs_allowed_admin_html() ); */ ?>
-			<?php echo wp_kses( $urvenue_ws_pagesingleevent, urvenue_ws_adm_allowed_admin_html() );  // Axl UWS-7416 ?>
-			<?php // @Axl End ?>
+			<?php echo wp_kses( $urvenue_ws_pagesingleevent, urvenue_ws_adm_allowed_admin_html() ); ?>
 		</div>
 	</div>
     <div class="uvs-infolist-item uvs-clearfix">
-		<?php /* Old: echo wp_kses( $urvenue_ws_maptext, uvs_allowed_admin_html() ); */ ?>
-		<div class="uvsname">Map Page<?php /* Old: echo $urvenue_ws_maptext; */ echo wp_kses( $urvenue_ws_maptext, urvenue_ws_adm_allowed_admin_html() );  // Axl UWS-7416 ?></div>
+		<div class="uvsname">Map Page<?php echo wp_kses( $urvenue_ws_maptext, urvenue_ws_adm_allowed_admin_html() ); ?></div>
 		<div class="uvsvalue">
-			<?php // @Axl ?>
-			<?php /* Old: echo $urvenue_ws_pagemap; */ ?>
-			<?php /* Old: echo wp_kses( $urvenue_ws_pagemap, uvs_allowed_admin_html() ); */ ?>
-			<?php echo wp_kses( $urvenue_ws_pagemap, urvenue_ws_adm_allowed_admin_html() );  // Axl UWS-7416 ?>
-			<?php // @Axl End ?>
+			<?php echo wp_kses( $urvenue_ws_pagemap, urvenue_ws_adm_allowed_admin_html() ); ?>
 		</div>
 	</div>
 	<div class="uvs-infolist-item uvs-clearfix">
-		<?php /* Old: echo wp_kses( $urvenue_ws_itempagetext, uvs_allowed_admin_html() ); */ ?>
-		<div class="uvsname">Inventory Item Page<?php /* Old: echo $urvenue_ws_itempagetext; */ echo wp_kses( $urvenue_ws_itempagetext, urvenue_ws_adm_allowed_admin_html() );  // Axl UWS-7416 ?></div>
+		<div class="uvsname">Inventory Item Page<?php echo wp_kses( $urvenue_ws_itempagetext, urvenue_ws_adm_allowed_admin_html() ); ?></div>
 		<div class="uvsvalue">
-			<?php // @Axl ?>
-			<?php /* Old: echo $urvenue_ws_pageitem; */ ?>
-			<?php /* Old: echo wp_kses( $urvenue_ws_pageitem, uvs_allowed_admin_html() ); */ ?>
-			<?php echo wp_kses( $urvenue_ws_pageitem, urvenue_ws_adm_allowed_admin_html() );  // Axl UWS-7416 ?>
-			<?php // @Axl End ?>
+			<?php echo wp_kses( $urvenue_ws_pageitem, urvenue_ws_adm_allowed_admin_html() ); ?>
 		</div>
 	</div>
 	<div class="uvs-infolist-item uvs-clearfix">
 		<div class="uvsname">Privacy Page</div>
 		<div class="uvsvalue">
-			<?php // @Axl ?>
-			<?php /* Old: echo $urvenue_ws_pagepriv; */ ?>
-			<?php /* Old: echo wp_kses( $urvenue_ws_pagepriv, uvs_allowed_admin_html() ); */ ?>
-			<?php echo wp_kses( $urvenue_ws_pagepriv, urvenue_ws_adm_allowed_admin_html() );  // Axl UWS-7416 ?>
-			<?php // @Axl End ?>
+			<?php echo wp_kses( $urvenue_ws_pagepriv, urvenue_ws_adm_allowed_admin_html() ); ?>
 		</div>
 	</div>
 	<div class="uvs-infolist-item uvs-clearfix">
 		<div class="uvsname">Terms Page</div>
 		<div class="uvsvalue">
-			<?php // @Axl ?>
-			<?php /* Old: echo $urvenue_ws_pageterms; */ ?>
-			<?php /* Old: echo wp_kses( $urvenue_ws_pageterms, uvs_allowed_admin_html() ); */ ?>
-			<?php echo wp_kses( $urvenue_ws_pageterms, urvenue_ws_adm_allowed_admin_html() );  // Axl UWS-7416 ?>
-			<?php // @Axl End ?>
+			<?php echo wp_kses( $urvenue_ws_pageterms, urvenue_ws_adm_allowed_admin_html() ); ?>
 		</div>
 	</div>
 </div>
